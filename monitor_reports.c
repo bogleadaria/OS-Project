38ba5ab7bde58f45e1dcd3a7e5c651271f50d05b
Boglea Daria <bogleadaria@gmail.com>
2026-05-04 19:29:30 +0300

    added something to monitor_reports.c

--- monitor_reports.c
+++ monitor_reports.c
@@ -2,18 +2,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-
-static void handle_sigusr1(int sig){
+static void handle_sigusr1(int sig)
+{
     (void)sig;
     const char *msg = "[monitor] New report added.\n";
     write(STDOUT_FILENO, msg, strlen(msg));
 }
 
-static void handle_sigint(int sig){
+static void handle_sigint(int sig)
+{
     (void)sig;
     unlink(PID_FILE);
-    const char *msg="[monitor] SIGINT received. Shutting down.\n";
+    const char *msg = "[monitor] SIGINT received. Shutting down.\n";
     write(STDOUT_FILENO, msg, strlen(msg));
+}
+
+void write_pid_file(void)
+{
+    int f = open(PID_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (f == -1)
+    {
+        perror("Failed to create .monitor_pid");
+        _exit(1);
+    }
+    char pid_str[32];
+    int len = snprintf(pid_str, "%d\n", (int)getpid);
+
+    write(f, pid_str, len);
+
+    close(f);
 }
